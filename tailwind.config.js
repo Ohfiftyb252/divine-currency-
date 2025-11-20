2188a995da9a361ab34c310ee95f80568641519f
Ohfiftyb252 <89036566+Ohfiftyb252@users.noreply.github.com>
2025-11-20 13:09:42 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,10 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./frontend/pages/**/*.{js,ts,jsx,tsx}",
-    "./frontend/components/**/*.{js,ts,jsx,tsx}"
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: [
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/typography")
-  ]
-};
+  plugins: [],
+}
